e1724f0732cbbae02342ef216fab195c74349f4c
Nanda Arya Putra <nandaarya404@gmail.com>
2022-12-13 21:01:51 +0700

    add insert function to database

--- models/korban.php
+++ models/korban.php
@@ -1,8 +1,16 @@
 <?php
 include 'connection.php';
-class Model extends Connection{
-public function __construct()
+class Model extends Connection
+{
+    public function __construct()
     {
         $this->conn = $this->get_connection();
     }
+
+    public function insert($idx, $nama_korban, $hubungan, $lokasi, $tanggal, $alasan_hilang, $detail, $nama_pelapor, $telepon, $email)
+    {
+        $sql = "INSERT INTO korban (idx, nama_korban, hubungan, lokasi, tanggal, alasan_hilang, detail, nama_pelapor, telepon, email) VALUES ('$idx', '$nama_korban', '$hubungan', '$lokasi', '$tanggal', '$alasan_hilang', '$detail', '$nama_pelapor', '$telepon', '$email')";
+        $this->conn->query($sql);
+    }
+
 }
---
+++ models/process.php
@@ -0,0 +1,19 @@
+<?php
+include 'korban.php';
+if (isset($_POST['submit_simpan'])) {
+    $idx = $_POST['idx'];
+    $nama_korban = $_POST['nama_korban'];
+    $hubungan = $_POST['hubungan'];
+    $lokasi = $_POST['lokasi'];
+    $tanggal = $_POST['tanggal'];
+    $alasan_hilang = $_POST['alasan_hilang'];
+    $detail = $_POST['detail'];
+    // $foto = $_POST['foto'];
+    $nama_pelapor = $_POST['nama_pelapor'];
+    $telepon = $_POST['telepon'];
+    $email = $_POST['email'];
+
+    $model = new Model();
+    $model->insert($idx, $nama_korban, $hubungan, $lokasi, $tanggal, $alasan_hilang, $detail, $nama_pelapor, $telepon, $email);
+    header('location:index.php');
+}
