4959a10e069448b6dc3cff60b11b6605db0aea49
Nanda Arya Putra <nandaarya404@gmail.com>
2022-12-14 18:31:45 +0700

    add login view

---
+++ views/login.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Lapor</title>
+</head>
+
+<body>
+    <form method="post">
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" class="form-control" name="username" id="username" placeholder="Username">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+        </div>
+        <button type="submit" name="submit" class="btn btn-primary">Login</button>
+    </form>
+</body>
+
+</html>
