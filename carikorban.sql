94cd9d718b40cb2fa212c49c1c2348dbb9da5aaf
Nanda Arya Putra <nandaarya404@gmail.com>
2022-12-27 12:14:39 +0700

    add comment

--- carikorban.sql
+++ carikorban.sql
@@ -3,6 +3,12 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `korban`
+--
+
 CREATE TABLE `login` (
   `id` int(11) NOT NULL,
   `username` varchar(30) NOT NULL,
