dabeef45336c512a61e1388bb7a7ce6b3e50f94e
Nanda Arya Putra <nandaarya404@gmail.com>
2023-01-26 20:59:11 +0700

    format code

--- views/dataditemukan.php
+++ views/dataditemukan.php
@@ -13,7 +13,8 @@ $index = 1;
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Data Ditemukan</title>
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- google fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -28,41 +29,44 @@ $index = 1;
         <!-- <h1><?php echo $_SESSION['role'] ?></h1> -->
 
         <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand logo" href="#">CariKorban</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="lapor.php">Lapor</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="datahilang.php">Data Hilang</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="dataditemukan.php">Data Ditemukan</a>
-                    </li>
-                </ul>
-                <?php
+            <div class="container-fluid">
+                <a class="navbar-brand logo" href="#">CariKorban</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="lapor.php">Lapor</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="datahilang.php">Data Hilang</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="dataditemukan.php">Data Ditemukan</a>
+                        </li>
+                    </ul>
+                    <?php
                     if ($_SESSION['role'] == 'Admin') { ?>
-                    <a href="logout.php"><button type="button" class="d-flex btn btn-danger my-2 my-sm-0">Logout</button></a>
-                    <?php
+                        <a href="logout.php"><button type="button"
+                                class="d-flex btn btn-danger my-2 my-sm-0">Logout</button></a>
+                        <?php
                     } else { ?>
-                    <a href="login.php"><button type="button" class="d-flex btn btn-primary my-2 my-sm-0">Login</button></a>
-                    <?php
+                        <a href="login.php"><button type="button"
+                                class="d-flex btn btn-primary my-2 my-sm-0">Login</button></a>
+                        <?php
                     } ?>
+                </div>
             </div>
-        </div>
         </nav>
 
         <br>
         <h4>Data Korban Ditemukan</h4>
         <br>
-        
+
         <form action="dataditemukan.php" method="get">
-        <label class="form-label">Cari Dalam Data Korban Ditemukan</label>
+            <label class="form-label">Cari Dalam Data Korban Ditemukan</label>
             <div class="input-group my-1" style="width: 40%;">
                 <br>
                 <input class="form-control" type="search" name="cari" placeholder="Masukkan nama...">
@@ -82,11 +86,10 @@ $index = 1;
                     <th>Detail Korban</th>
                     <th>Nama Pelapor</th>
                     <th>Status</th>
-                    <?php if ($_SESSION['role'] == 'Guest') { ?> 
-                            <th><a title="Lihat Foto">Foto Korban</a></th>
-                    <?php }
-                        else { ?> 
-                            <th><a title="Operasi">Operasi</a></th>
+                    <?php if ($_SESSION['role'] == 'Guest') { ?>
+                        <th><a title="Lihat Foto">Foto Korban</a></th>
+                    <?php } else { ?>
+                        <th><a title="Operasi">Operasi</a></th>
                     <?php } ?>
                 </tr>
             </thead>
@@ -100,58 +103,57 @@ $index = 1;
                 }
                 if (!empty($result)) {
                     foreach ($result as $data): ?>
-                <tr>
-                    <td>
-                        <?= $index++ ?>
-                    </td>
-                    <td>
-                        <?= $data->nama_korban ?>
-                    </td>
-                    <td>
-                        <?= $data->hubungan ?>
-                    </td>
-                    <td>
-                        <?= $data->lokasi ?>
-                    </td>
-                    <td>
-                        <?= $data->tanggal ?>
-                    </td>
-                    <td>
-                        <?= $data->alasan_hilang ?>
-                    </td>
-                    <td>
-                        <?= $data->detail ?>
-                    </td>
-                    <td>
-                        <?= $data->nama_pelapor ?>
-                    </td>
-                    <td>
-                        <?= $data->status ?>
-                    </td>
-                    <td>
-                        <?php if ($_SESSION['role'] == 'Guest') { ?>
-                            <a title="Lihat Foto" name="foto" id="foto" target="_blank" rel="noopener noreferrer"
-                            href="lihatfoto.php?idx=<?= $data->idx ?>"><i
-                            class="fa fa-picture-o" style="font-size:14px;color:blue"></i></a>
-                        <?php } 
-                        else { ?>
-                                <a title="Lihat Foto" name="foto" id="foto" target="_blank" rel="noopener noreferrer"
-                                    href="lihatfoto.php?idx=<?= $data->idx ?>"><i
-                                    class="fa fa-picture-o" style="font-size:14px;color:blue"></i></a>
-                                |
-                                <a title="Ubah" name="edit" id="edit" href="edit.php?idx=<?= $data->idx ?>"><i
-                                    class="fa fa-edit" style="font-size:14px;color:orange"></i></a>
-                                |
-                                <a title="Hapus" name="hapus" id="hapus" href="..\models\process.php?idx=<?= $data->idx ?>"><i
-                                    class="fa fa-trash-o" style="font-size:14px;color:red"></i></a>
-                           <?php } ?>
-                    </td>
-                </tr>
-                <?php endforeach;
+                        <tr>
+                            <td>
+                                <?= $index++ ?>
+                            </td>
+                            <td>
+                                <?= $data->nama_korban ?>
+                            </td>
+                            <td>
+                                <?= $data->hubungan ?>
+                            </td>
+                            <td>
+                                <?= $data->lokasi ?>
+                            </td>
+                            <td>
+                                <?= $data->tanggal ?>
+                            </td>
+                            <td>
+                                <?= $data->alasan_hilang ?>
+                            </td>
+                            <td>
+                                <?= $data->detail ?>
+                            </td>
+                            <td>
+                                <?= $data->nama_pelapor ?>
+                            </td>
+                            <td>
+                                <?= $data->status ?>
+                            </td>
+                            <td>
+                                <?php if ($_SESSION['role'] == 'Guest') { ?>
+                                    <a title="Lihat Foto" name="foto" id="foto" target="_blank" rel="noopener noreferrer"
+                                        href="lihatfoto.php?idx=<?= $data->idx ?>"><i class="fa fa-picture-o"
+                                            style="font-size:14px;color:blue"></i></a>
+                                <?php } else { ?>
+                                    <a title="Lihat Foto" name="foto" id="foto" target="_blank" rel="noopener noreferrer"
+                                        href="lihatfoto.php?idx=<?= $data->idx ?>"><i class="fa fa-picture-o"
+                                            style="font-size:14px;color:blue"></i></a>
+                                    |
+                                    <a title="Ubah" name="edit" id="edit" href="edit.php?idx=<?= $data->idx ?>"><i
+                                            class="fa fa-edit" style="font-size:14px;color:orange"></i></a>
+                                    |
+                                    <a title="Hapus" name="hapus" id="hapus" href="..\models\process.php?idx=<?= $data->idx ?>"><i
+                                            class="fa fa-trash-o" style="font-size:14px;color:red"></i></a>
+                                <?php } ?>
+                            </td>
+                        </tr>
+                    <?php endforeach;
                 } else { ?>
-                <tr>
-                    <td colspan="9">Belum ada data korban ditemukan.</td>
-                </tr>
+                    <tr>
+                        <td colspan="9">Belum ada data korban ditemukan.</td>
+                    </tr>
                 <?php } ?>
             </tbody>
         </table>
@@ -191,8 +193,9 @@ $index = 1;
             margin: 0 30px;
         }
 
-        tr:hover {background-color: lightgray;}
-
+        tr:hover {
+            background-color: lightgray;
+        }
     </style>
 </body>
 
