d7efe7a5d1eea5c91f271780c86a718069efa5d8
Nanda Arya Putra <nandaarya404@gmail.com>
2023-01-27 23:06:12 +0700

    format code

--- connection.php
+++ connection.php
@@ -1,7 +1,9 @@
 <?php
-abstract class Connection{
+abstract class Connection
+{
     // database account information (don't forget to change it for security reason)
-    protected function get_connection(){
+    protected function get_connection()
+    {
         $host = "localhost";
         $database = "carikorban";
         $username = "root";
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 header("Location: views/datahilang.php");
 exit()
-?>
+    ?>
