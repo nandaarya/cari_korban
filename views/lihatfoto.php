4d6f1ccd8e6fd1a4c395febf62e7d2e5447f0234
Nanda Arya Putra <nandaarya404@gmail.com>
2023-02-05 22:09:45 +0700

    edit sql

--- views/lihatfoto.php
+++ views/lihatfoto.php
@@ -8,11 +8,13 @@ if (!empty($result)) {
   foreach ($result as $data):
     if ($data->idx == $idx) {
       if ($data->file_gambar == null) { ?>
-        <?Php echo "<h2>Tidak ada foto tersedia</h2>" ?>        <?php
-                   echo '<img src="..\assets\images\foto_kosong.jpg" width="200px" height="200px"/>';
+        <?Php echo "<h2>Tidak ada foto tersedia</h2>" ?>
+        <?php
+        echo '<img src="..\assets\images\foto_kosong.jpg" width="200px" height="200px"/>';
       } else { ?>
-        <?Php echo "<h2>Foto $data->nama_korban</h2>" ?>        <?php
-                  echo '<img src="data:image/jpeg;base64,' . base64_encode($data->file_gambar) . '" width="200px" height="200px"/>';
+        <?Php echo "<h2>Foto $data->nama_korban</h2>" ?>
+        <?php
+        echo '<img src="data:image/jpeg;base64,' . base64_encode($data->file_gambar) . '" width="200px" height="200px"/>';
       }
     }
   endforeach;
